e56a7511f49724860716c60e7a2ff30a3d3f53cb
matteobiagiola <biagiolamatteo2@gmail.com>
2022-11-09 09:39:50 +0100

    Revised model prediction and processing; add new dave2 model and remove the old one

--- self_driving/image_processing.py
+++ self_driving/image_processing.py
@@ -1,39 +1,40 @@
 import cv2
 import numpy as np
-import matplotlib.image as mpimg
-
 
 IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS = 160, 320, 3
 INPUT_SHAPE = (IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS)
 
 
-def crop(image):
+def crop(image: np.ndarray) -> np.ndarray:
     """
     Crop the image (removing the sky at the top and the car front at the bottom)
     """
-    return image[80:-1, :, :] # remove the sky and the car front
+    return image[80:-30, :, :]  # remove the sky and the car front
 
 
-def resize(image):
+def resize(image: np.ndarray) -> np.ndarray:
     """
     Resize the image to the input shape used by the network model
     """
     return cv2.resize(image, (IMAGE_WIDTH, IMAGE_HEIGHT), cv2.INTER_AREA)
 
 
-def rgb2yuv(image):
+def rgb2yuv(image: np.ndarray) -> np.ndarray:
     """
     Convert the image from RGB to YUV (This is what the NVIDIA model does)
     """
     return cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
 
 
-def preprocess(image):
+def preprocess(image: np.ndarray, normalize: bool = False) -> np.ndarray:
     """
     Combine all preprocess functions into one
     """
-    image = crop(image)
-    image = resize(image)
-    image = rgb2yuv(image)
-    return image
+    image = crop(image=image)
+    image = resize(image=image)
+    image = rgb2yuv(image=image)
 
+    if normalize:
+        image = (image / 127.5) - 1.0
+
+    return image
--- self_driving/nvidia_prediction.py
+++ self_driving/nvidia_prediction.py
@@ -3,7 +3,10 @@ import numpy as np
 from self_driving.simulation_data import SimulationDataRecord
 from self_driving.image_processing import preprocess
 
-MIN_SPEED = 10
+import tensorflow as tf
+
+MIN_SPEED = 20
+
 
 class NvidiaPrediction:
     def __init__(self, model, max_speed):
@@ -11,21 +14,23 @@ class NvidiaPrediction:
         self.speed_limit = max_speed
         self.max_speed = max_speed
 
-    def predict(self, image, car_state: SimulationDataRecord):
+    def predict(self, image, car_state: SimulationDataRecord, normalize: bool = False):
         try:
             image = np.asarray(image)
 
-            image = preprocess(image)
+            image = preprocess(image=image, normalize=normalize)
             image = np.array([image])
 
-            steering_angle = float(self.model.predict(image, batch_size=1))
+            with tf.device('/cpu:0'):
+                steering_angle = float(self.model.predict(image, batch_size=1))
 
             speed = car_state.vel_kmh
             if speed > self.speed_limit:
                 self.speed_limit = MIN_SPEED  # slow down
             else:
                 self.speed_limit = self.max_speed
-            throttle = 1.0 - steering_angle ** 2 - (speed / self.speed_limit) ** 2
+
+            throttle = np.clip(a=1.0 - steering_angle ** 2 - (speed / self.speed_limit) ** 2, a_min=0.0, a_max=1.0)
             return steering_angle, throttle
 
         except Exception as e:
