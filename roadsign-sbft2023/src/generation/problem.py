f4cde5fa84b56e87303f277d74f46c54a03a6d54
matteobiagiola <biagiolamatteo2@gmail.com>
2023-02-02 19:43:20 +0100

    Setting up EVOMBT, SPIRALE and ROADSIGN tools

---
+++ roadsign-sbft2023/src/generation/problem.py
@@ -0,0 +1,132 @@
+import numpy as np
+
+from pymoo.core.problem import ElementwiseProblem
+
+'''
+Multi-Objective Black-Box Test Case Selection for Cost-Effectively Testing Simulation Models
+Arrieta et al. 2018
+Instability + Discontinuity + Growth to Infinity, based on the turning angles of the road.
+Fitness values are normalized over road length, preventing roads with not much going on in large sections.
+'''
+
+
+def segment_angles(segments, step=10):
+    # Memoize traversed road distance for each segment
+    l = np.empty(len(segments))
+    l[0] = segments[0].length
+    for i in range(1, len(segments)):
+        l[i] = l[i - 1] + segments[i].length
+    li = 0
+    # Prepare step count and output array
+    length = sum(s.length for s in segments)
+    k = int(length / step)
+    a = np.empty(k)
+    # Compute values for each step
+    a[0] = 0
+    for i in range(1, k):
+        while l[li] < i * step:
+            li += 1
+        segment = segments[li]
+        a[i] = a[0] + (step / segment.radius)
+    return a
+
+
+def instability(signal):
+    return sum((signal[i] - signal[i - 1] for i in range(1, len(signal))))
+
+
+def discontinuity(signal):
+    return (
+        max((
+            max((
+                min(abs(signal[i] - signal[i - dt]), abs(signal[i + dt] - signal[i]))
+                for i in range(dt, len(signal) - dt)
+            ))
+            for dt in [1, 2, 3]
+        ))
+    )
+
+
+def growth(signal):
+    return np.max(np.abs(signal))
+
+
+def on_segment(p, q, r):
+    return all((
+        (q[0] <= max(p[0], r[0])),
+        (q[0] >= min(p[0], r[0])),
+        (q[1] <= max(p[1], r[1])),
+        (q[1] >= min(p[1], r[1])),
+    ))
+
+
+def orientation(p, q, r):
+    return np.sign(((q[1] - p[1]) * (r[0] - q[0])) - ((q[0] - p[0]) * (r[1] - q[1])))
+
+
+def lines_intersect(a, b):
+    o1 = orientation(a[0], a[1], b[0])
+    o2 = orientation(a[0], a[1], b[1])
+    o3 = orientation(b[0], b[1], a[0])
+    o4 = orientation(b[0], b[1], a[1])
+    return any((
+        (o1 != o2) and (o3 != o4),
+        (o1 == 0) and on_segment(a[0], b[0], a[1]),
+        (o2 == 0) and on_segment(a[0], b[1], a[1]),
+        (o3 == 0) and on_segment(b[0], a[0], b[1]),
+        (o4 == 0) and on_segment(b[0], a[1], b[1]),
+    ))
+
+
+def path_self_intersects(path):
+    path = list(((p[0], p[1]) for p in path))
+    start_lines = {}
+    end_lines = {}
+    for p1, p2 in zip(path, path[1:]):
+        if p1[0] > p2[0]:
+            p1, p2 = p2, p1
+        line = (p1, p2)
+        start_lines[p1] = line
+        end_lines[p2] = line
+    bucket = set()
+    x_sorted = sorted(path, key=lambda p: p[0])
+    for point in x_sorted:
+        e = end_lines.get(point)
+        if e is not None:
+            bucket.discard(e)
+        s = start_lines.get(point)
+        if s is not None:
+            if any(map(lambda l: lines_intersect(l, s), bucket)):
+                return True
+            bucket.add(s)
+    return False
+
+
+def is_valid(road):
+    return not path_self_intersects(road.points)
+
+
+class TestGenerationProblem(ElementwiseProblem):
+
+    def __init__(self, rng: np.random.Generator, segments_range, length_range, total_length_range, radius_range,
+                 map_size):
+        super().__init__(n_var=1, n_obj=3)
+        self.rng = rng
+        self.segments_range = segments_range
+        self.length_range = length_range
+        self.total_length_range = total_length_range
+        self.radius_range = radius_range
+        self.map_size = map_size
+        self.i = 0
+
+    def _evaluate(self, x, out, *args, **kwargs):
+        if is_valid(x):
+            angles = segment_angles(x.segments)
+            length = x.length()
+            out["F"] = (
+                -instability(angles),
+                -discontinuity(angles),
+                -growth(angles),
+            )
+        else:
+            out["F"] = 0.0, 0.0, 0.0
---
+++ roadsign-sbft2023/src/roadsign_generator.py
@@ -0,0 +1,143 @@
+import logging as log
+from copy import deepcopy
+from time import time_ns
+
+import numpy as np
+
+from code_pipeline.tests_generation import RoadTestFactory
+
+from src.generation.optimize import Optimizer, get_termination, POP_SIZE
+from src.road import Road, segments_distance
+
+
+def min_distance(roads, length_range, radius_range):
+    return min(min(segments_distance(
+        roads[i].segments, roads[j].segments,
+        length_range=length_range, radius_range=radius_range
+    ) for j in range(i + 1, len(roads))
+                   ) for i in range(len(roads) - 1))
+
+
+def sample_roads(roads, samples, initial_sample, length_range, radius_range, threshold=0.0):
+    if len(roads) <= samples:
+        return roads
+    sampled = {initial_sample}
+    for _ in range(samples - 1):
+        best_candidate = None
+        best_diversity = threshold
+        for index in range(len(roads)):
+            if index not in sampled:
+                diversity = min(
+                    segments_distance(
+                        roads[index].segments, roads[s].segments,
+                        length_range=length_range, radius_range=radius_range
+                    ) for s in filter(lambda s: s != index, sampled)
+                )
+                if diversity > best_diversity:
+                    best_diversity = diversity
+                    best_candidate = index
+        if best_candidate is not None:
+            sampled.add(best_candidate)
+    return [roads[i] for i in sampled]
+
+
+class RoadSignGenerator():
+    """
+    Generates roads in two steps:
+    1. Random diverse roads are seeded as an initial population.
+    2. A genetic algorithm evolves the population in order to maximize the following features:
+        * Instability
+        * Discontinuity
+        * Growth
+    For Step 2, a signal is generated for each road, based on periodic samples of its facing angle.
+    """
+
+    def __init__(self, executor, map_size=200, segments_range=(10, 30), length_range=None, total_length_range=None,
+                 radius_range=(30, 100), gen_max_time=60, seed_samples=10, gen_samples=10):
+        self.executor = executor
+        # Set default values based on map_size
+        if length_range is None:
+            length_range = (map_size / 20, map_size / 4)
+        if total_length_range is None:
+            total_length_range = (map_size, map_size * 4)
+        self.rng = np.random.Generator(np.random.MT19937(seed=time_ns()))
+        self.segments_range = segments_range
+        self.length_range = length_range
+        self.total_length_range = total_length_range
+        self.radius_range = radius_range
+        self.map_size = map_size
+        # self.termination = get_termination('n_gen', 300)
+        self.termination = get_termination('time', gen_max_time)
+        self.seed_samples = seed_samples
+        self.gen_samples = gen_samples
+
+    def start(self):
+        log.info("Starting RoadSignGenerator")
+        while not self.executor.is_over():
+            try:
+                # Initialize optimizer
+                optimizer = Optimizer(
+                    rng=self.rng,
+                    segments_range=self.segments_range,
+                    length_range=self.length_range,
+                    total_length_range=self.total_length_range,
+                    radius_range=self.radius_range,
+                    map_size=self.map_size,
+                    termination=self.termination,
+                )
+                log.info("[DIVERSE_ROADS] Start")
+                seeded_roads = optimizer.sampling.get_or_generate_samples(
+                    problem=optimizer.problem, n_samples=POP_SIZE,
+                )
+                seeded_roads = deepcopy(sample_roads(
+                    roads=seeded_roads,
+                    samples=self.seed_samples,
+                    initial_sample=self.rng.integers(low=0, high=len(seeded_roads)),
+                    length_range=self.length_range,
+                    radius_range=self.radius_range,
+                    threshold=0.0,
+                ))
+                threshold = min_distance(
+                    roads=seeded_roads,
+                    length_range=self.length_range,
+                    radius_range=self.radius_range,
+                ) / 2
+                self.execute_roads(seeded_roads, prefix='[DIVERSE_ROADS] ')
+                log.info("[DIVERSE_ROADS] Done")
+                if self.executor.is_over():
+                    break
+                log.info("[OPTIMIZED_ROADS] Start")
+                results = optimizer.run()
+                optimized_roads = [
+                    Road.from_segments(rng=self.rng, segments=r.segments, map_size=self.map_size) for r in results.X
+                ]
+                if len(optimized_roads) > self.gen_samples:
+                    optimized_roads = sample_roads(
+                        roads=optimized_roads,
+                        samples=self.gen_samples,
+                        initial_sample=self.rng.integers(low=0, high=len(optimized_roads)),
+                        length_range=self.length_range,
+                        radius_range=self.radius_range,
+                        threshold=threshold,
+                    )
+                log.info(f"[OPTIMIZED_ROADS] Generated tests: {len(optimized_roads)}")
+                self.execute_roads(optimized_roads, prefix='[OPTIMIZED_ROADS] ')
+            except:
+                raise  # log.exception('Exception in RoadSignGenerator')
+        log.info("Finished RoadSignGenerator")
+
+    def execute_roads(self, roads, prefix=''):
+        log.info(f"{prefix}Starting test execution")
+        for i, road in enumerate(roads, start=1):
+            log.info(f"{prefix}Executing test {i}/{len(roads)}")
+            self.execute_road(road)
+        log.info(f"{prefix}Finished test execution")
+
+    def execute_road(self, road):
+        points = list((float(p[0]), float(p[1])) for p in road.points)
+        log.info("Generated test from road points %s", points)
+        the_test = RoadTestFactory.create_road_test(points)
+        test_outcome, description, execution_data = self.executor.execute_test(the_test)
+        log.info("Test outcome: {}. Description: {}".format(test_outcome, description))
+        log.info("Remaining Time: %s", str(self.executor.get_remaining_time()["time-budget"]))
+        return execution_data
